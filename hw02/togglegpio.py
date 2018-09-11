d3b90342bed1172849e4c922877a3254e4812d66
Russmania101 <42875598+Russmania101@users.noreply.github.com>
2018-09-11 15:59:54 -0400

    added header

--- hw02/togglegpio.py
+++ hw02/togglegpio.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+# Russell Johnson
+# ECE434
+# Homework 2
+
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
 pin = "P9_12"
 GPIO.setup(pin, GPIO.OUT)
+
 #delay = 0.05 # 100ms
 #delay = 0.005
 #delay = 0.0005
