520edb05261c93ed96655bba4d89a267c962d092
Russmania101 <42875598+Russmania101@users.noreply.github.com>
2018-09-11 16:01:50 -0400

    added header

--- hw03/encoder.py
+++ hw03/encoder.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
+
 # From: https://adafruit-beaglebone-io-
-#python.readthedocs.io/en/latest/Encoder.html
+# python.readthedocs.io/en/latest/Encoder.html
+
+# Russell Johnson
+# ECE434
+# Homework 2
+
 from Adafruit_BBIO.Encoder import RotaryEncoder, eQEP2
 import time
+
 # Instantiate the class to access channel eQEP2, and initialize
 # that channel
 myEncoder = RotaryEncoder(eQEP2)
 myEncoder.setAbsolute()
 myEncoder.enable()
+
 # Get the current position
 while True:
     print(myEncoder.position)
