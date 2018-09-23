69125f795a328dc8a8e454643c07472dacd0fbd1
Russell Johnson <johnsor2@rose-hulman.edu>
2018-09-23 14:18:41 -0500

    text

--- hw04/text.sh
+++ hw04/text.sh
@@ -6,7 +6,7 @@ TMP_FILE=/tmp/frame.png
 # From: http://www.imagemagick.org/Usage/text/
 convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
       -size $SIZE \
-      label:'ImageMagick\nExamples\nby Anthony' \
+      label:'Russell Johnson\n' \
       -draw "text 0,200 'Bottom of Display'" \
       $TMP_FILE
 
