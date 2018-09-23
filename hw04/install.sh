97c2aed679d906f335afef1972b8a05edea7f601
Russell Johnson <johnsor2@rose-hulman.edu>
2018-09-23 13:30:56 -0500

    update readme

--- hw04/install.sh
+++ hw04/install.sh
@@ -2,3 +2,14 @@
 wget http://free-electrons.com/pub/mirror/devmem2.c
 gcc -o devmem2 devmem2.c
 sudo mv devmem2 /usr/bin
+
+
+# Install fbi
+sudo apt-get update
+sudo apt-get install fbi
+
+# Install mplayer
+sudo apt-get install mplayer
+
+# Install imagemagick
+sudo apt-get install imagemagick
