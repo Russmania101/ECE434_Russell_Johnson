41dfde9687ce3eac631074e4fa38838ad291ee3b
Russell Johnson <johnsor2@rose-hulman.edu>
2018-10-16 15:25:55 -0400

    added gpio3

--- hw09/hello.c
+++ hw09/hello.c
@@ -3,14 +3,16 @@
 #include "resource_table_empty.h"
 
 #define GPIO1 0x4804C000
+#define GPIO3 0x481AE000
 #define GPIO_CLEARDATAOUT 0x190
 #define GPIO_SETDATAOUT 0x194
 #define USR0 (1<<21)
 #define USR1 (1<<22)
 #define USR2 (1<<23)
 #define USR3 (1<<24)
-unsigned int volatile * const GPIO1_CLEAR = (unsigned int *) (GPIO1 + GPIO_CLEARDATAOUT);
-unsigned int volatile * const GPIO1_SET   = (unsigned int *) (GPIO1 + GPIO_SETDATAOUT);
+#define P9_27 (1<<19)
+unsigned int volatile * const GPIO3_CLEAR = (unsigned int *) (GPIO3 + GPIO_CLEARDATAOUT);
+unsigned int volatile * const GPIO3_SET   = (unsigned int *) (GPIO3 + GPIO_SETDATAOUT);
 
 volatile register unsigned int __R30;
 volatile register unsigned int __R31;
@@ -22,10 +24,10 @@ void main(void) {
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
 	for(i=0; i<5; i++) {
-		*GPIO1_SET = USR3;      // The the USR3 LED on
+		*GPIO3_SET = P9_27;      // The the USR3 LED on
 		__delay_cycles(500000000/5);    // Wait 1/2 second
 
-		*GPIO1_CLEAR = USR3;
+		*GPIO3_CLEAR = :P9_27;
 		 __delay_cycles(500000000/5); 
 	}
 	__halt();
